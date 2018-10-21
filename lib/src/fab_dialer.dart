c0d91467d763b9621bb41aa7d217cf281b5c11ed
xia-weiyang <gw@jiushig.com>
2018-10-21 17:06:48 +0800

    Add floatButton onLongPress method

--- lib/src/fab_dialer.dart
+++ lib/src/fab_dialer.dart
@@ -2,25 +2,26 @@ part of fab_dialer;
 
 class FabDialer extends StatefulWidget {
   const FabDialer(this._fabMiniMenuItemList, this._fabColor, this._fabIcon,
-    [this._closeFabIcon, this._animationDuration]);
+      [this._closeFabIcon,this.onLongPress, this._animationDuration]);
 
   final List<FabMiniMenuItem> _fabMiniMenuItemList;
   final Color _fabColor;
   final Icon _fabIcon;
   final Icon _closeFabIcon;
   final int _animationDuration;
+  final VoidCallback onLongPress;
 
 
   @override
   FabDialerState createState() =>
-    FabDialerState(
-      _fabMiniMenuItemList, _fabColor, _fabIcon, _closeFabIcon,
-      _animationDuration);
+      FabDialerState(
+          _fabMiniMenuItemList, _fabColor, _fabIcon, _closeFabIcon,
+          _animationDuration);
 }
 
 class FabDialerState extends State<FabDialer> with TickerProviderStateMixin {
   FabDialerState(this._fabMiniMenuItemList, this._fabColor, this._fabIcon,
-    this._closeFabIcon, this._animationDuration);
+      this._closeFabIcon, this._animationDuration);
 
   bool _isRotated = false;
   final List<FabMiniMenuItem> _fabMiniMenuItemList;
@@ -35,8 +36,8 @@ class FabDialerState extends State<FabDialer> with TickerProviderStateMixin {
   @override
   void initState() {
     final int animationDuration = _animationDuration == null
-      ? 180
-      : _animationDuration;
+        ? 180
+        : _animationDuration;
     _controller = AnimationController(
       vsync: this,
       duration: Duration(milliseconds: animationDuration),
@@ -81,39 +82,42 @@ class FabDialerState extends State<FabDialer> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     final Icon closeIcon = _closeFabIcon == null
-      ? Icon(Icons.close)
-      : _closeFabIcon;
+        ? Icon(Icons.close)
+        : _closeFabIcon;
     return Container(
-      margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: <Widget>[
-          Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: _fabMenuItems,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget>[
-              AnimatedBuilder(
-                animation: _controller,
-                builder: (BuildContext _, Widget child) {
-                  return FloatingActionButton(
-                    child: Transform(
-                      transform: Matrix4.rotationZ(
-                        (2 * Math.pi) * _controller.value),
-                      alignment: Alignment.center,
-                      child: _controller.value >= 0.5
-                        ? closeIcon
-                        : _fabIcon,
-                    ),
-                    backgroundColor: _fabColor,
-                    onPressed: _rotate);
-                },
-              )
-            ],
-          ),
-        ],
-      ));
+        margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: _fabMenuItems,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                AnimatedBuilder(
+                  animation: _controller,
+                  builder: (BuildContext _, Widget child) {
+                    return GestureDetector(
+                      child: FloatingActionButton(
+                          child: Transform(
+                            transform: Matrix4.rotationZ(
+                                (2 * Math.pi) * _controller.value),
+                            alignment: Alignment.center,
+                            child: _controller.value >= 0.5
+                                ? closeIcon
+                                : _fabIcon,
+                          ),
+                          backgroundColor: _fabColor,
+                          onPressed: _rotate),
+                      onLongPress: widget.onLongPress,
+                    );
+                  },
+                )
+              ],
+            ),
+          ],
+        ));
   }
 }
